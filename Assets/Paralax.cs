58cd9ab211e0b5a64012ba41d105d63fd0a8f42d
mbradley36 <mirandajbradley@gmail.com>
2015-10-06 20:19:27 -0700

    subtle sky movemen

---
+++ Assets/Paralax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralax : MonoBehaviour {
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = gameObject.transform.position;
+		pos.x = Time.time * moveSpeed;
+		gameObject.transform.position = pos;
+	}
+}
